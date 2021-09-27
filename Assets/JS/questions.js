4d8d73e4d533131540480b2e56162fab23ffad47
tehshaw <25413002+tehshaw@users.noreply.github.com>
2021-09-26 22:49:55 -0400

    code completed
    -project is working including HTML, CSS and JS files

--- Assets/JS/questions.js
+++ Assets/JS/questions.js
@@ -1,65 +1,71 @@
 var questions =[
     {
-        question: "Question 1",
-        answers: ["This may be the answer to the question", 
-        "This may be the answer to the question", 
-        "This may be the answer to the question",
-         "This may be the answer to the question"],
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["The Equalizer", "Manchester by the Sea", 
+        "Guardians of the Galaxy", "The Bourne Identity"],
+        answerKey: "3"
+     
+    },
+    {
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["Multiplicity", "Happy Feet", 
+        "Inception", "Ocean's Thirteen"],
+        answerKey: "3"
+     
+    },
+    {
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["The Quick and the Dead", "JFK", 
+        "Saving Private Ryan", "Training Day"],
         answerKey: "2"
      
     },
     {
-        question: "Question 2",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["To Die For", "21 Jump Street", 
+        "Four Brothers", "The Martian"],
+        answerKey: "3"
      
     },
     {
-        question: "Question 3",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["Good WIll Hunting", "Mightnight in Paris",
+        "The Aviator", "Pearl Harbor"],
+        answerKey: "0"
      
     },
     {
-        question: "Question 4",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["We Bought a Zoo", "Paycheck", 
+        "The Last of the Mohicans", "Angels & Demons"],
+        answerKey: "0"
      
     },
     {
-        question: "Question 5",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["Scream", "Invictus", 
+        "Team America: World Police", "Minority Report"],
+        answerKey: "1"
      
     },
     {
-        question: "Question 6",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["The Hateful Eight", "Being John Malkovich",
+        "Milk", "The Departed"],
+        answerKey: "3"
      
     },
     {
-        question: "Question 7",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["Ocean's 8", "THe Dark Knight", 
+        "The Monuments Men", "Two Weeks Notice"],
+        answerKey: "2"
      
     },
     {
-        question: "Question 8",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
+        question: "Which movie did Matt Damon appear in?",
+        answers: ["Downsizing", "Primal Fears", 
+        "Spy Kids", "Elysium"],
+        answerKey: "3"
      
-    },
-    {
-        question: "Question 9",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
-     
-    },
-    {
-        question: "Question 10",
-        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-        answerKey: "Answer 2"
-     
-    }
-]
+    }]
--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -3,6 +3,9 @@ var timeCount = document.getElementById("theTime");
 var startBtn = document.getElementById("startButton");
 var answersSpace = document.getElementById("qList");
 var enterNameBoard = document.getElementById("endGame");
+var submitForm = document.getElementById("enter-name")
+
+var yourName = "";
 
 var timeleft = 0;
 
@@ -34,17 +37,17 @@ function askQuestion(){
     answersSpace.innerHTML = "";
 
     for(let i = 0; i < theQuestions.length; i++){
-        var nextQuestion = document.createElement("LI");
-        nextQuestion.setAttribute("class", "purple")
+        var nextAnswer = document.createElement("LI");
+        nextAnswer.setAttribute("class", "purple")
 
         var answerButton = document.createElement("button");
-        answerButton.setAttribute("class", "purple")
-        answerButton.dataset.number = i;
+        answerButton.setAttribute("class", "purple answer-button")
+        answerButton.setAttribute("index", i);
         answerButton.textContent = theQuestions[i];
-
-        nextQuestion.appendChild(answerButton);
-        answersSpace.appendChild(nextQuestion);
+        nextAnswer.appendChild(answerButton);
+        answersSpace.appendChild(nextAnswer);
     }
+           
     questionNumberIndex++;
 
 }
@@ -52,9 +55,8 @@ function askQuestion(){
 
 function startTimer(){
 
-    if(timeCount.textContent !== 75){
-        timeleft = 75;
-    }
+    //give 10 seconds per question
+    timeleft = questions.length * 10;
 
     countDown = setInterval(() => {
         timeCount.textContent = timeleft;
@@ -62,7 +64,7 @@ function startTimer(){
         
         if(timeleft == 0){
             stopTimer();
-            gameOver(false);
+            gameOver();
         }
     }, 1000);
 
@@ -72,45 +74,93 @@ function stopTimer(){
     clearInterval(countDown);
 }
 
+//when called will hide he questions, whether complete or time ran out
+//then show the final screen to enter user name for scoreboard
 function gameOver(){
     questionArea.setAttribute("class", "hide");
     stopTimer();
     enterNameBoard.removeAttribute("class");
+    timeCount.textContent ="Game Over"
+    document.getElementById("your-score").textContent = timeleft;
 }
 
-//listener to check answers for questions
+//fucntion to take user name and highscore and put to local
+function endGame(){
+    
+    //ensures the user actaully entered a name
+    if(yourName === ""){
+        alert("Please enter a name.");
+        return;
+    }
+
+    //grab the highscores from the local storage
+    var highScores = JSON.parse(localStorage.getItem("allScores"));
+
+    //if there are no highscores stored locally yet 
+    //then set highScores to an empty string to prevent an error
+    if (highScores == null){
+        highScores = [];
+    }
+
+    //add user score to the array of scores as an object
+    highScores.push({name: yourName, score: timeleft});
+
+    //sort the arry by score in decending order
+    highScores.sort( (x , y) => y.score - x.score);
+
+    //place arary with new score sorted back into storage
+    localStorage.setItem("allScores", JSON.stringify(highScores));
+    
+    //load highscores page of site
+    window.open("highScores.html", "_self");
+
+}
+
+//this is the listener for the UL list when the user selects an answer
 questionArea.addEventListener("click", event => {
     var element = event.target;
 
-    if(element.tagName = "BUTTON"){
-        if(element.dataset.number === currentQuestion.answerKey){
-            element.setAttribute("class", "green")
+    if(element.tagName === "BUTTON"){
+        console.log(element.textContent);
+        var buttons = document.querySelectorAll("button[index]");
+
+        if(element.getAttribute("index") === currentQuestion.answerKey){
+            element.setAttribute("class", "green answer-button")
         }else{
-            element.setAttribute("class", "red")
+            element.setAttribute("class", "red answer-button")
+            buttons[currentQuestion.answerKey].setAttribute("class", "green answer-button");
             timeleft -= 10;
         }
+        if(questionNumberIndex >= questions.length || timeleft <= 0){
+            setTimeout(() => {
+                gameOver();
+            }, 750);
+        }else{
+            setTimeout(() => {
+                askQuestion();
+            }, 750);
+        }
     }
-    if(questionNumberIndex > questions.length || timeleft <= 0){
-        gameOver();
-    }else{
-        setTimeout(() => {
-            askQuestion();
-        }, 500);
-    }
+
 
 });
 
-
+//this listne is for the start button on the start screen
+//when pressed will "start" the quiz by calling func starQuiz
 startBtn.addEventListener("click", startQuiz);
 
-var submitForm = document.getElementById("enter-name")
 
+//listen event to watch for form submit when user hit enter
+//will grab user name from the text box and time remaining
 submitForm.addEventListener("submit", (event) =>{
     event.preventDefault();
-    var yourName =  document.getElementById("name-text").textContent;
-
-
-    
-
+    yourName =  document.getElementById("name-text").value.trim();
+    endGame();
 })
+
+
+
+
+
+
 
